5a966e0e269297f4298d7b8d8d8ff3280a3db024
komaksym <kovalmaksym2@gmail.com>
2025-06-23 23:51:05 +0200

    finish first elastic net frame that transforms l1 and l2 into elastic

--- manimations/elastic_net.py
+++ manimations/elastic_net.py
@@ -4,23 +4,62 @@ config.background_color = WHITE  # or "#FFFFFF"
 
 class ElasticNetFormula(Scene):
     def construct(self):
-        # Title
-        title = Text("Elastic Net Regularization", color="black")
-        title.scale(1.2)
-        self.add(title.to_edge(UP, buff=1))
+        """L1"""
+         # Title
+        l1_title = Text("L1 Regularization", color="black")
+        l1_title.scale(0.5)
+        self.add(l1_title.to_edge(UP, buff=0.5))
 
         # Formula
-        tex = MathTex(r"cost = loss\ term + \lambda_{1}\sum_{j=1}^{k}\left|w_{j}\right| +\lambda_{2}\sum_{j=1}^{k}w_{j}^{2}",
-                       font_size=50, color='black')
-        tex.scale(1.2)
-        self.add(tex)
+        l1_formula = MathTex(
+            r"cost = loss\ term + \lambda\sum_{j=1}^{k}\left|w_{j}\right|", 
+            font_size=50, color="black", 
+            substrings_to_isolate=["cost = loss\ term +", "\lambda\sum_{j=1}^{k}\left|w_{j}\right|"]
+        )
+        l1_formula.scale(0.7)
+        self.add(l1_formula.to_edge(UP, buff=1))
+    
+
+        """L2"""
+        # Title
+        l2_title = Text("L2 Regularization", color="black")
+        l2_title.scale(0.5)
+        self.add(l2_title.to_edge(UP, buff=2.5))
+
+        # Formula
+        l2_formula = MathTex(r"cost = loss\ term + \lambda\sum_{j=1}^{k}w_{j}^{2}",
+                              font_size=50, color="black", 
+                              substrings_to_isolate=["", "cost = loss\ term +", "\lambda\sum_{j=1}^{k}w_{j}^{2}"])
+        l2_formula.scale(0.7)
+        self.add(l2_formula.to_edge(UP, buff=3))
+
+
+        """Elastic Net"""
+        # Title
+        elastic_title = Text("Elastic Net Regularization", color="black")
+        elastic_title.scale(0.7)
+        self.play(Write(elastic_title.to_edge(UP, buff=4.5)), run_time=2)
+
+        # Formula
+        elastic_net = MathTex(r"cost = loss\ term + \lambda_{1}\sum_{j=1}^{k}\left|w_{j}\right| +\lambda_{2}\sum_{j=1}^{k}w_{j}^{2}",
+                       color='black', substrings_to_isolate=["cost = loss\ term +", "\lambda_{1}\sum_{j=1}^{k}\left|w_{j}\right|", "\lambda_{2}\sum_{j=1}^{k}w_{j}^{2}"])
+        elastic_net.to_edge(UP, buff=5.0)
+
+        # Transformations
+        self.play(Transform(l1_formula[0], elastic_net[0]), Transform(l2_formula[0], elastic_net[0]), run_time=3)
+        self.play(Transform(l1_formula[1], elastic_net[1]), run_time=3)
+        self.play(Transform(l2_formula[2], elastic_net[2]), run_time=3)
+
+        elastic_net.scale(0.7)
+        #self.play(Write(elastic_net)
+
 
 
         # Rectangle highlighting the regularization term
-        rectangle = Rectangle(color=ORANGE, fill_opacity=0, 
-                              width=6.2, height=2.2, stroke_width=5)
-        rectangle.shift(RIGHT * 2.7)
+        #rectangle = Rectangle(color=ORANGE, fill_opacity=0, 
+        #                      width=6.2, height=2.2, stroke_width=5)
+        #rectangle.shift(RIGHT * 2.7)
 
-        # Render the rectangle
-        self.play(Create(rectangle, run_time=2.5))
+        ## Render the rectangle
+        #self.play(Create(rectangle, run_time=2.5))
         self.wait(10)
